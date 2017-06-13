f854fb602cd23ffd3b246b084b34eeff99f7aa0a
2046 <g@ggg.gd>
2017-06-13 17:01:56 +0800

    init

---
+++ dist/index.js
@@ -0,0 +1,141 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+exports.default = function (options = {}) {
+    options = (0, _deepmerge2.default)((0, _deepmerge2.default)({}, DEFAULT_OPTIONS), options);
+
+    let cspValue, hstsValue, xframeValue;
+
+    if (options.csp.enabled) {
+        let key, value;
+
+        cspValue = [];
+
+        for (key of Object.keys(options.csp)) {
+            if (key === 'enabled' || key === 'report') {
+                continue;
+            }
+
+            value = getCspValue(options.csp[key]);
+            key = decamelize(key);
+            cspValue.push(` ${key} ${value}`);
+        }
+
+        cspValue = cspValue.join(';').trim();
+    }
+
+    if (options.hsts.enabled) {
+        hstsValue = `max-age=${options.hsts.maxAge}`;
+
+        if (options.hsts.includeSubDomains) {
+            hstsValue += '; includeSubDomains';
+        }
+    }
+
+    if (options.xframe.enabled) {
+        if (options.xframe.deny) {
+            xframeValue = 'DENY';
+        } else if (options.xframe.origin) {
+            xframeValue = 'SAMEORIGIN';
+        } else if (options.xframe.allowUrl) {
+            xframeValue = `ALLOW-FROM ${options.xframe.allowUrl}`;
+        }
+    }
+
+    return async function _security(ctx, next) {
+        if (options.ip.enabled && !(0, _ipFilter2.default)(ctx.ip, options.ip.filter)) {
+            ctx.status = 403;
+            return;
+        }
+
+        if (options.csp.enabled) {
+            if (options.csp.report) {
+                ctx.set('Content-Security-Policy-Report-Only', cspValue);
+            } else {
+                ctx.set('Content-Security-Policy', cspValue);
+            }
+        }
+
+        if (options.hsts.enabled) {
+            ctx.set('Strict-Transport-Security', hstsValue);
+        }
+
+        if (options.noopen.enabled) {
+            ctx.set('X-Download-Options', 'noopen');
+        }
+
+        if (options.nosniff.enabled) {
+            ctx.set('X-Content-Type-Options', 'nosniff');
+        }
+
+        if (options.xframe.enabled) {
+            ctx.set('X-Frame-Options', xframeValue);
+        }
+
+        if (options.xss.enabled) {
+            ctx.set('X-XSS-Protection', '1; mode=block');
+        }
+
+        await next();
+    };
+};
+
+var _ipFilter = require('ip-filter');
+
+var _ipFilter2 = _interopRequireDefault(_ipFilter);
+
+var _deepmerge = require('deepmerge');
+
+var _deepmerge2 = _interopRequireDefault(_deepmerge);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Thanks to:
+//   - https://developer.mozilla.org/zh-CN/docs/Web/Security/CSP/Using_Content_Security_Policy
+//   - https://developer.mozilla.org/zh-CN/docs/Security/HTTP_Strict_Transport_Security
+//   - https://msdn.microsoft.com/zh-cn/library/jj542450(v=vs.85).aspx
+//   - https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/X-Content-Type-Options
+//   - https://developer.mozilla.org/zh-CN/docs/Web/HTTP/X-Frame-Options
+//   - https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/X-XSS-Protection
+
+const DEFAULT_OPTIONS = {
+    ip: {
+        filter: [],
+        enabled: false
+    },
+    csp: {
+        report: false,
+        enabled: false
+    },
+    hsts: {
+        enabled: false,
+        includeSubDomains: false,
+        maxAge: 365 * 24 * 60 * 60
+    },
+    noopen: {
+        enabled: false
+    },
+    nosniff: {
+        enabled: false
+    },
+    xframe: {
+        deny: false,
+        origin: false,
+        enabled: false,
+        allowUrl: false
+    },
+    xss: {
+        enabled: false
+    }
+};
+
+function getCspValue(values) {
+    return values.join(' ').replace(/self/ig, "'self'").replace(/none/ig, "'none'").replace(/unsafe-eval/ig, "'unsafe-eval'").replace(/unsafe-inline/ig, "'unsafe-inline'");
+}
+
+function decamelize(str) {
+    return str.split(/(?=[A-Z])/).join('-').toLowerCase();
+}
---
+++ tests/index.js
@@ -0,0 +1,233 @@
+import koa from 'koa'
+import test from 'ava'
+import security from '../src'
+import request from 'request'
+import mount from 'koa-mount'
+
+const req = request.defaults({ json: true })
+
+test.cb('ip filter, no match filter rule, statusCode is 403', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        ip: { enabled: true, filter: ['192.168.1.1'] }
+    }))
+
+    app.listen(3000, '0.0.0.0')
+
+    req.get('http://localhost:3000', (err, res, body) => {
+        t.is(res.statusCode, 403)
+        t.end()
+    })
+})
+
+test.cb('ip filter, match filter rule, statusCode is 200', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        ip: { enabled: true, filter: ['127.0.0.1'] }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3001, '0.0.0.0')
+
+    req.get('http://localhost:3001', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.end()
+    })
+})
+
+test.cb('Content-Security-Policy', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        csp: {
+            enabled: true,
+            defaultSrc: ['self'],
+            objectSrc: ['none'],
+            scriptSrc: ['unsafe-eval', 'unsafe-inline', 'https://test.com']
+        }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(4000)
+
+    req.get('http://localhost:4000', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['content-security-policy'], "default-src 'self'; object-src 'none'; script-src 'unsafe-eval' 'unsafe-inline' https://test.com")
+        t.end()
+    })
+})
+
+test.cb('Content-Security-Policy-Report-Only', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        csp: {
+            report: true,
+            enabled: true,
+            defaultSrc: ['self'],
+            objectSrc: ['none'],
+            scriptSrc: ['unsafe-eval', 'unsafe-inline', 'https://test.com'],
+            reportUri: ['http://test.com']
+        }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(4001)
+
+    req.get('http://localhost:4001', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['content-security-policy-report-only'], "default-src 'self'; object-src 'none'; script-src 'unsafe-eval' 'unsafe-inline' https://test.com; report-uri http://test.com")
+        t.end()
+    })
+})
+
+test.cb('Strict-Transport-Security', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        hsts: { enabled: true, includeSubDomains: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3002)
+
+    req.get('http://localhost:3002', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['strict-transport-security'], 'max-age=31536000; includeSubDomains')
+        t.end()
+    })
+})
+
+test.cb('X-Download-Options', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        noopen: { enabled: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3003)
+
+    req.get('http://localhost:3003', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-download-options'], 'noopen')
+        t.end()
+    })
+})
+
+test.cb('X-Content-Type-Options', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        nosniff: { enabled: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3004)
+
+    req.get('http://localhost:3004', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-content-type-options'], 'nosniff')
+        t.end()
+    })
+})
+
+test.cb('X-Frame-Options, value is DENY', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        xframe: { enabled: true, deny: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3005)
+
+    req.get('http://localhost:3005', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-frame-options'], 'DENY')
+        t.end()
+    })
+})
+
+test.cb('X-Frame-Options, value is SAMEORIGIN', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        xframe: { enabled: true, origin: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3006)
+
+    req.get('http://localhost:3006', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-frame-options'], 'SAMEORIGIN')
+        t.end()
+    })
+})
+
+test.cb('X-Frame-Options, value is SAMEORIGIN', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        xframe: { enabled: true, allowUrl: 'http://test.com' }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3007)
+
+    req.get('http://localhost:3007', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-frame-options'], 'ALLOW-FROM http://test.com')
+        t.end()
+    })
+})
+
+test.cb('X-XSS-Protection', (t) => {
+    let app = new koa()
+
+    app.use(security({
+        xss: { enabled: true }
+    }))
+    app.use(mount('/', async function(ctx, next) {
+        ctx.body = { ok: 1 }
+    }))
+
+    app.listen(3008)
+
+    req.get('http://localhost:3008', (err, res, body) => {
+        t.is(res.statusCode, 200)
+        t.deepEqual(body, { ok: 1 })
+        t.is(res.headers['x-xss-protection'], '1; mode=block')
+        t.end()
+    })
+})
